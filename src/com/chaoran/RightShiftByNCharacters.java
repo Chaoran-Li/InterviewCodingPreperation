f1d5921227e49c94287ff950678d7194fc4de0d9
Chaoran <lichaoran520@gmail.com>
2017-09-05 15:05:48 -0400

    First commit

---
+++ src/com/chaoran/RightShiftByNCharacters.java
@@ -0,0 +1,29 @@
+package com.chaoran;
+
+/**
+ * Created by chaoranli on 9/5/17.
+ */
+public class RightShiftByNCharacters {
+
+    public String rightShift(String input, int n) {
+        if (input == null) {
+            return null;
+        }
+
+        char[] array = input.toCharArray();
+        reverse(array, 0, array.length - 1);
+        reverse(array, 0, n - 1);
+        reverse(array, n, array.length - 1);
+        return new String(array);
+    }
+
+    private void reverse(char[] array, int left, int right) {
+        while (left <= right) {
+            char temp = array[left];
+            array[left] = array[right];
+            array[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
